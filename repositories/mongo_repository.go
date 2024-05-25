f5ea3e15e64633b5e3fbafac848c634ffbd7bb66
code-raushan <code-raushan@users.noreply.github.com>
2024-05-25 14:22:53 +0530

    add: fetch response query

--- repositories/mongo_repository.go
+++ repositories/mongo_repository.go
@@ -1,9 +1,16 @@
 package repositories
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"log"
+
+	"github.com/code-raushan/go-mongodb-server/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type mongoRepo struct {
-	client *mongo.Client
+	client     *mongo.Client
 	collection *mongo.Collection
 }
 
@@ -11,7 +18,55 @@ func NewMongoRepo(client *mongo.Client, dbName string, collectionName string) *m
 	collection := client.Database(dbName).Collection(collectionName)
 
 	return &mongoRepo{
-		client: client,
+		client:     client,
 		collection: collection,
 	}
+}
+
+func (m *mongoRepo) Fetch(filter types.FilterOptions) []types.FetchResponse {
+	ctx := context.Background()
+
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{
+				"$and": []bson.M{
+					{"createdAt": bson.M{"$gte": filter.StartDate}},
+					{"createdAt": bson.M{"$lte": filter.EndDate}},
+				},
+			},
+		},
+		{
+			"$addFields": bson.M{
+				"totalCount": bson.M{"$sum": "$counts"},
+			},
+		},
+		{
+			"$match": bson.M{
+				"$and": []bson.M{
+					{"totalCount": bson.M{"$gte": filter.MinCount}},
+					{"totalCount": bson.M{"$lt": filter.MaxCount}},
+				},
+			},
+		},
+		{"$project": bson.M{
+			"key":        1,
+			"createdAt":  1,
+			"totalCount": 1,
+		}},
+	}
+
+	cur, err := m.collection.Aggregate(ctx, pipeline)
+
+	if err != nil {
+		log.Fatalf("Error while fetching data records %v", err)
+	}
+
+	defer cur.Close(ctx)
+
+	var results []types.FetchResponse
+	if err := cur.All(ctx, &results); err != nil {
+		log.Fatalf("Error while storing data records into response: %v", err)
+	}
+
+	return results
 }
